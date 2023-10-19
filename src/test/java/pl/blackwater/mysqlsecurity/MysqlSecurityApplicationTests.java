84e745298b57c3e2e727fd16ae9fcdcc374d8d70
CzarnaWoda <38265586+CzarnaWoda@users.noreply.github.com>
2023-10-19 09:33:20 +0200

    Update

---
+++ src/test/java/pl/blackwater/mysqlsecurity/MysqlSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.blackwater.mysqlsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
